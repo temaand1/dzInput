900057922434bd3259951251258f9602d1364c75
temaand1 <45510216+temaand1@users.noreply.github.com>
2018-12-21 15:50:08 +0300

    Almost all forms

---
+++ gulpFile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./style/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./style/**/*.scss', ['sass']);
+});
